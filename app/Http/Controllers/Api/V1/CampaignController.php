48206415438247928a1c0e33d2d51b6e657c8e9f
Kien NV <vankien9x@gmail.com>
2018-04-02 12:00:11 +0700

    Save statistic sms

--- app/Http/Controllers/Api/V1/CampaignController.php
+++ app/Http/Controllers/Api/V1/CampaignController.php
@@ -335,19 +335,17 @@ class CampaignController extends ApiController
             if (!count($campaign->sms)) {
                 return $this->infoResponse([]);
             }
-            $smsApi = new SpeedSMSAPI();
-            $smsReport = [];
-            foreach ($campaign->sms as $smsPack) {
-                $result = $smsApi->getSMSStatus($smsPack->sms_id);
-                if ($result['status'] == 'success') {
-                    $smsReport = array_merge($smsReport, $result['data']);
-                }
-            }
+            $smsReport = $this->campaign->statisticSms($campaign);
             return $this->infoResponse($smsReport);
         }
         return $this->notFoundResponse();
     }
 
+    /**
+     * Danh sách tin nhắn phản hồi của khách hàng
+     * @param  Request $request
+     * @return [type]           [description]
+     */
     public function smsIncoming(Request $request)
     {
         $params = $request->all();
@@ -403,6 +401,8 @@ class CampaignController extends ApiController
 
         try {
             $data->cgroup()->delete();
+            $data->customers()->detach();
+            $data->sms()->delete();
             $this->getResource()->delete($id);
 
             DB::commit();
