02e6acc95980eb5a95a60bf92688c883edaeb34c
Saturn Lai <daolvcntt@gmail.com>
2018-06-09 11:26:02 +0700

    tag

--- app/Repositories/Tags/DbTagRepository.php
+++ app/Repositories/Tags/DbTagRepository.php
@@ -10,4 +10,13 @@ class DbTagRepository extends BaseRepository implements TagRepository
         $this->model = $tag;
     }
 
+    public function store($params)
+    {
+        $lists = ['#229954', '#B7950B', '#196F3D', '#7D3C98', '#E74C3C', '#3498DB'];
+        $key                = array_rand($lists);
+        $params['slug']     = str_slug($params['name'], '-');
+        $params['color']    = $lists[$key];
+        $model              = $this->model->create($params);
+        return $this->getById($model->id);
+    }
 }
